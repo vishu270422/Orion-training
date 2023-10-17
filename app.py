5c2cae5b0c8a8341fdd87ab65084713b084ffa0f
vishu270422 <129824992+vishu270422@users.noreply.github.com>
2023-10-17 21:38:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, session, redirect
+from functools import wraps
+import pymongo
+
+app = Flask(__name__)
+app.secret_key = "pass@1234"
+
+
+client = pymongo.MongoClient('localhost', 27017)
+db = client.user_login_system
+
+def login_required(f):
+  @wraps(f)
+  def wrap(*args, **kwargs):
+    if 'logged_in' in session:
+      return f(*args, **kwargs)
+    else:
+      return redirect('/')
+  
+  return wrap
+
+# Routes
+from user import routes
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+@app.route('/dashboard/')
+@login_required
+def dashboard():
+  return render_template('dashboard.html')
+
+
+
+if __name__ =="__main__":
+    app.run(debug=True)
